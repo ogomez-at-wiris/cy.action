2c708c176b6bf29bb535b1dc2501f68e32c64252
ogomez-at-wiris <oriol.gomez@wiris.com>
2022-06-10 08:51:34 +0200

    test: Add new action environment test

--- cypress/integration/test_action.spec.js
+++ cypress/integration/test_action.spec.js
@@ -4,6 +4,13 @@ describe('spec.cy.js', () => {
   it('should test the Github action', () => {
     cy.visit('https://www.oriolgomez.com')
     cy.get('ul > li:nth-child(1) > a')
-    .should('contain.text', 'Games for iOZ')
+    .should('contain.text', 'Games for iOS')
+  })
+
+  it('has valid env values', () => {
+    expect(Cypress.env())
+      .to.be.an('object')
+      .and.to.include.keys('env')
+    cy.log('**test:** ' + Cypress.env('env').test)
   })
 })
